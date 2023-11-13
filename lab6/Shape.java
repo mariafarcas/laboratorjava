182876a593ff8a92fef86d5caee481e5ff4fa75d
mariafarcas <farcas.mariaantonia@student.uoradea.ro>
2023-11-13 20:50:19 +0200

    lab6

---
+++ lab6/Shape.java
@@ -0,0 +1,26 @@
+package lab6;
+
+public abstract class Shape {
+    protected Integer l;
+
+    public Shape(Integer l){
+        this.l=l;
+    }
+
+    public abstract  Double area();
+    public abstract  Integer perimeter();
+
+    public void desen(){
+
+        System.out.println("Area of shape"+ area());
+        System.out.println("Perimeter of shape"+ perimeter());
+    }
+    public void desen(String culoare){
+
+        System.out.println("Area of shape"+ area());
+        System.out.println("Perimeter of shape"+ perimeter());
+        System.out.println("Colour of shape"+ culoare);
+    }
+
+    public static void afiseazaCeva(){System.out.println("Ceva"); }
+}
